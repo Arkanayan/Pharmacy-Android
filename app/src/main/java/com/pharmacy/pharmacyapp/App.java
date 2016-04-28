6498ee73ddc721157d2306ec030f6491a971d56c
Arka <itsarkanayan@gmail.com>
2016-04-28 09:37:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/pharmacy/pharmacyapp/App.java
@@ -0,0 +1,11 @@
+package com.pharmacy.pharmacyapp;
+
+import android.app.Application;
+
+/**
+ * Created by arka on 4/28/16.
+ */
+public class App extends Application {
+
+
+}
---
+++ app/src/main/java/com/pharmacy/pharmacyapp/InitialActivity.java
@@ -0,0 +1,12 @@
+package com.pharmacy.pharmacyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
