c89ae8ad6ac33502834b83e8bb24131daf55762a
Arka <itsarkanayan@gmail.com>
2016-04-28 10:55:12 +0530

    Initialize digits

--- app/src/main/java/com/pharmacy/pharmacyapp/App.java
+++ app/src/main/java/com/pharmacy/pharmacyapp/App.java
@@ -3,12 +3,18 @@ package com.pharmacy.pharmacyapp;
 import android.app.Application;
 import android.content.Context;
 
+import com.digits.sdk.android.Digits;
 import com.firebase.client.Firebase;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by arka on 4/28/16.
  */
 public class App extends Application {
+
 
     private static Firebase mFirebase;
     private static Context mContext;
@@ -18,11 +24,21 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // Initialize Firebase
         Firebase.setAndroidContext(this);
-
+        // Firebase.getDefaultConfig().setPersistenceEnabled(true);
         mFirebase = new Firebase(getString(R.string.firebase_url));
 
         mContext = this;
+
+        // Fabric initialization
+        final String TWITTER_KEY = getString(R.string.twitter_key);
+        final String TWITTER_SECRET = getString(R.string.twitter_secret);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits());
+
+
     }
 
     public static Firebase getFirebase() {
--- app/src/main/java/com/pharmacy/pharmacyapp/InitialActivity.java
+++ app/src/main/java/com/pharmacy/pharmacyapp/InitialActivity.java
@@ -1,7 +1,7 @@
 package com.pharmacy.pharmacyapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class InitialActivity extends AppCompatActivity {
 
