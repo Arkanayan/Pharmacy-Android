d791d051feeb0184f3140d3a765da7920495ae3e
Arka <itsarkanayan@gmail.com>
2016-04-28 09:47:35 +0530

    Initialize global app and firebase

--- app/src/main/java/com/pharmacy/pharmacyapp/App.java
+++ app/src/main/java/com/pharmacy/pharmacyapp/App.java
@@ -1,11 +1,35 @@
 package com.pharmacy.pharmacyapp;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.firebase.client.Firebase;
 
 /**
  * Created by arka on 4/28/16.
  */
 public class App extends Application {
 
+    private static Firebase mFirebase;
+    private static Context mContext;
 
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Firebase.setAndroidContext(this);
+
+        mFirebase = new Firebase(getString(R.string.firebase_url));
+
+        mContext = this;
+    }
+
+    public static Firebase getFirebase() {
+        return mFirebase;
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
 }
